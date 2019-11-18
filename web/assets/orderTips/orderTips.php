0614b8b18a236cd6df66652f000b76fe57865b75
luffy522 <1065431774@qq.com>
2019-11-18 14:08:10 +0800

    work

---
+++ web/assets/orderTips/orderTips.php
@@ -0,0 +1,5 @@
+
+<?php
+    echo "<audio src=\"http://www.lmeri.com/assets/admin/dist/remind.mp3\" autoplay=\"autoplay\"></audio><script>setTimeout('window.close()','4500')</script>";
+?>
+<!DOCTYPE html><html><head><meta charset="UTF-8"><title>提示音</title></head><body>您有新的消息！</body></html>
---
+++ web/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// [ 应用入口文件 ]
+
+// 定义网站域名
+define('DOMAIN_NAME', 'http://www.lmeri.com/');
+
+// 定义运行目录
+define('WEB_PATH', __DIR__ . '/');
+
+// 定义应用目录
+define('APP_PATH', WEB_PATH . '../source/application/');
+
+//定义图片路径常量
+define('BIG_IMG', DOMAIN_NAME.'web/uploads/big/');     //大图
+define('SIM_IMG', DOMAIN_NAME.'web/uploads/small/');     //小图
+
+// 加载框架引导文件
+require APP_PATH . '../thinkphp/start.php';
